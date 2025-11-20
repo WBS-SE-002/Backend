403e5227b7c9ceaad7c76108a244edb886fbc979
Garrett Lander <garrett.jlander@gmail.com>
2025-11-20 11:39:30 +0100

    async/await lecture

---
+++ 01-advanced-csharp/03-AsyncAwait-Lecture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+static async Task<object> FetchDataAsync(string url)
+{
+  using var httpClient = new HttpClient();
+
+  var data = await httpClient.GetStringAsync(url);
+  return data;
+}
+
+// var ducks = await FetchDataAsync("https://duckpond-89zn.onrender.com/wild-ducks");
+// Console.WriteLine(ducks);
+
+static async Task DelayedLogAsync(string message)
+{
+  await Task.Delay(500);
+  // throw new NotImplementedException("You didn't write this yet?");
+  Console.WriteLine(message);
+}
+
+// await DelayedLogAsync("I'm slow...");
+
+// try
+// {
+//   await DelayedLogAsync("Delayed hello");
+// }
+// catch (Exception ex)
+// {
+//   Console.WriteLine($"Error: {ex.Message}");
+// }
+
+var stopWatch = Stopwatch.StartNew();
+Console.WriteLine("Hello");
+Console.WriteLine($"Sync elapsed: {stopWatch.ElapsedMilliseconds} ms\n");
+
+
+// var firstTask = DelayedLogAsync("Delayed hello");
+// await DelayedLogAsync("Another delayed hello");
+// await firstTask;
+
+var tasks = new[] { DelayedLogAsync("Delayed hello"), DelayedLogAsync("Another delayed hello") };
+
+await Task.WhenAll(tasks);
+
+Console.WriteLine($"Async elapsed: {stopWatch.ElapsedMilliseconds} ms\n");
