92b765f1b4a9b47586001bd946ab021e44ae6388
Garrett Lander <garrett.jlander@gmail.com>
2025-12-01 12:14:22 +0100

    efcore lecture

---
+++ 02-chapter24-asp.net/week2-db-and-auth/01-EFCore-Lecture/Services/UserService.cs
@@ -0,0 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+
+using BlogApi.Models;
+using BlogApi.Infrastructure;
+
+namespace BlogApi.Services;
+
+public class UserService : IUserService
+{
+  private readonly ApplicationDbContext _db;
+
+  public UserService(ApplicationDbContext db)
+  {
+    _db = db;
+  }
+  public async Task<User?> GetAsync(Guid id)
+  {
+    return await _db.Users.FindAsync(id);
+  }
+
+  public async Task<IReadOnlyList<User>> ListAsync()
+  {
+    return await _db.Users.ToListAsync();
+  }
+
+  public async Task<User> CreateAsync(string name, string email)
+  {
+    var user = new User
+    {
+      Id = Guid.NewGuid(),
+      Name = name,
+      Email = email,
+      CreatedAt = DateTimeOffset.UtcNow
+    };
+
+    _db.Users.Add(user);
+    await _db.SaveChangesAsync();
+    return user;
+  }
+
+  public async Task<User?> UpdateAsync(Guid id, string? name, string? email)
+  {
+    var user = await _db.Users.FindAsync(id);
+
+    if (user is null)
+      return null;
+
+    if (name is not null)
+      user.Name = name;
+
+    if (email is not null)
+      user.Email = email;
+
+    await _db.SaveChangesAsync();
+    return user;
+  }
+
+  public async Task<bool> DeleteAsync(Guid id)
+  {
+    var user = await _db.Users.FindAsync(id);
+
+    if (user is null)
+      return false;
+
+    _db.Users.Remove(user);
+    await _db.SaveChangesAsync();
+    return true;
+  }
+}
